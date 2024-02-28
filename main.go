66fa71ab158658df2084c1b0fa73909459b6bfba
Dimas <prawira.dimas.yudha@gmail.com>
2024-02-28 13:50:29 +0700

    update main.go

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import command "GoMage/cmd"
 
+func main() {
+	// This is the entry point for the application
+	// Create a new command
+	cmd := command.NewCommand()
+
+	// Run the command
+	cmd.Run()
 }
